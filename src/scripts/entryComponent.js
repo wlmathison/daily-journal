2aee346975757c7cc41c0798a3f843dae77df577
Billy Mathison <wlmathison@gmail.com>
2019-03-22 18:11:09 -0500

    adding event listener, collecting form values, creating factory function, creating method to post to API and chaing in with GET to display results to DOM

--- src/scripts/entryComponent.js
+++ src/scripts/entryComponent.js
@@ -1,13 +1,8 @@
-// const makeJournalEntryComponent = (journalEntry) => {
-//     // Create your own HTML structure for a journal entry
-//     return `<h2>${journalEntry.concepts}</h2><p>${journalEntry.entry}</p><p>${journalEntry.date}</p>
-//     `
-// }
-
 const buildingJournal = Object.create(null, {
+    // Function to build journal entry structure for posting to DOM
     makeJournalEntryComponent: {
         value: function (journalEntry) {
-            return `<h2>${journalEntry.concepts}</h2><p>${journalEntry.entry}</p><p>${journalEntry.date}</p>`
+            return `<h2>${journalEntry.concepts}</h2><p>${journalEntry.entry}</p><p>${journalEntry.date}</p><hr>`
         }
     }
 })
--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,8 +1,29 @@
 /*
     Main application logic that uses the functions and objects
     defined in the other JavaScript files.
-
-    Change the fake variable names below to what they should be
-    to get the data and display it.
 */
-API.getJournalEntries().then(renderToDOM.renderJournalEntries)
+
+const getEntriesThenRender = () => API.getJournalEntries().then(renderToDOM.renderJournalEntries);
+const saveJournalEntry = () => {
+    let newJournalEntry = createNewJournalEntry();
+    API.postJournalEntries(newJournalEntry).then(getEntriesThenRender);
+}
+getEntriesThenRender();
+
+// factory function to create new journal entry object
+const createNewJournalEntry = () => {
+    let entryDate = document.querySelector("#journalDate").value;
+    let entryConcept = document.querySelector("#conceptsCovered").value;
+    let entryEntry = document.querySelector("#journalEntry").value;
+    let entryMood = document.querySelector("#currentMood").value;
+    return {
+        "date": entryDate,
+        "concepts": entryConcept,
+        "entry": entryEntry,
+        "mood": entryMood
+    }
+}
+
+// creating journal entry save button
+const recordJournalEntryButton = document.getElementById("record-journal-entry");
+recordJournalEntryButton.addEventListener("click", saveJournalEntry);
