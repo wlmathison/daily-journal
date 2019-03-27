0d8530551050f6164470d54bda95f3e706efca8f
Billy Mathison <wlmathison@gmail.com>
2019-03-27 09:55:45 -0500

    completing challenge requirements. adding max limit to concepts field and regular expressions test for curse words

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -6,22 +6,32 @@
 const getEntriesThenRender = () => API.getJournalEntries().then(renderToDOM.renderJournalEntries);
 const saveJournalEntry = () => {
     let newJournalEntry = createNewJournalEntry();
-    API.postJournalEntries(newJournalEntry).then(getEntriesThenRender);
+    if (newJournalEntry !== false) {
+        API.postJournalEntries(newJournalEntry).then(getEntriesThenRender);
+    }
 }
 getEntriesThenRender();
 
 // factory function to create new journal entry object
 const createNewJournalEntry = () => {
+    let badWord = /shit|damn|fuck|ass|hell/i;
     let entryDate = document.querySelector("#journalDate").value;
     let entryConcept = document.querySelector("#conceptsCovered").value;
     let entryEntry = document.querySelector("#journalEntry").value;
     let entryMood = document.querySelector("#currentMood").value;
-    return {
-        "date": entryDate,
-        "concepts": entryConcept,
-        "entry": entryEntry,
-        "mood": entryMood
-    }
+    if (badWord.test(entryConcept)) {
+        alert("Your input contains a curse word. Please change your concept name.")
+        return false;
+    } else if (badWord.test(entryEntry)) {
+        alert("Your input contains a curse word. Please chage your entry.")
+        return false;
+    } else
+        return {
+            "date": entryDate,
+            "concepts": entryConcept,
+            "entry": entryEntry,
+            "mood": entryMood
+        }
 }
 
 // creating journal entry save button
