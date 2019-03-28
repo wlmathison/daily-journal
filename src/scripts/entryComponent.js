5cb627436220dd35f72fd19c99abd1df837a6677
Billy Mathison <wlmathison@gmail.com>
2019-03-28 16:01:10 -0500

    completing aprt 9. adding edit button, event handler, clearing entrylog, posting entry to edit to form, and creating PUT request to update object

--- src/scripts/entryComponent.js
+++ src/scripts/entryComponent.js
@@ -1,6 +1,17 @@
 const handleDelete = () => {
-    let sectionToDelete = event.target.parentNode.id;
+    let sectionToDelete = event.target.parentNode.id.split("--")[1];
     API.deleteJournalEntry(sectionToDelete).then(API.getJournalEntries).then(renderToDOM.renderJournalEntries)
+};
+
+const handleEdit = () => {
+    let sectionToEdit = event.target.parentNode.id.split("--")[1];
+    const entryLogRef = document.querySelector(".entryLog")
+    entryLogRef.textContent = "";
+    let updateButton = document.getElementById("record-journal-entry");
+    updateButton.textContent = "Update Journal Entry";
+    journalEntryEditForm(sectionToEdit);
+    // journalEntryEditForm(sectionToEdit);
+    // API.putJournalEntry(sectionToEdit).then(API.getJournalEntries).then(renderToDOM.renderJournalEntries)
 };
 
 const createDeleteButton = () => {
@@ -10,12 +21,20 @@ const createDeleteButton = () => {
     return button;
 };
 
+const createEditButton = (entry) => {
+    let button = document.createElement("button");
+    button.textContent = "Edit Entry";
+    button.id = entry;
+    button.addEventListener("click", handleEdit);
+    return button;
+};
+
 const buildingJournal = Object.create(null, {
     // Function to build journal entry structure for posting to DOM
     makeJournalEntryComponent: {
         value: function (journalEntry) {
             let entrySection = document.createElement("section");
-            entrySection.id = journalEntry.id;
+            entrySection.id = `section--${journalEntry.id}`;
             let entryH2 = document.createElement("h2");
             entryH2.textContent = journalEntry.concepts;
             entrySection.appendChild(entryH2);
@@ -26,6 +45,7 @@ const buildingJournal = Object.create(null, {
             entryP2.textContent = journalEntry.date;
             entrySection.appendChild(entryP2);
             entrySection.appendChild(createDeleteButton());
+            entrySection.appendChild(createEditButton(`edit-button--${journalEntry.id}`));
             let entryBreak = document.createElement("hr");
             entrySection.appendChild(entryBreak);
             return entrySection
